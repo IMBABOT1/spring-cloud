de0372d725ab2894e9a47bdd99793cfae5d09edb
unknown <zlovred12@gmail.com>
2022-11-10 23:29:08 +0300

    Initial commit

---
+++ product-service/src/main/java/ru/imbabot/cloud/ProductApp.java
@@ -0,0 +1,12 @@
+package ru.imbabot.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApp.class, args);
+    }
+}
---
+++ product-service/src/main/java/ru/imbabot/cloud/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package ru.imbabot.cloud.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.imbabot.cloud.entities.Product;
+import ru.imbabot.cloud.services.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> findAll(){
+        return productService.findAll();
+    }
+
+}
