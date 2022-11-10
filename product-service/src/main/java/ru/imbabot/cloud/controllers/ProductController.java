140ad5081dc56718c96cf3fac1aa5102adaac999
unknown <zlovred12@gmail.com>
2022-11-10 23:42:38 +0300

    tem

--- product-service/src/main/java/ru/imbabot/cloud/controllers/ProductController.java
+++ product-service/src/main/java/ru/imbabot/cloud/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package ru.imbabot.cloud.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ProductController {
 
     private final ProductService productService;
